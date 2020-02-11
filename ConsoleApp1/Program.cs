f1c200f8114cd0226b5915508ba1477b96f4ec82
max-student <max-student@maxtrain.com>
2020-02-11 13:33:00 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+    internal class Program {
+        public Program() {
+            }
+
+        static void Main(string[] args) {
+            Console.WriteLine("Good morning!");
+            }
+        }
+    }
+namespace ConsoleApp1 {
+    internal class Program {
+            }
+
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome!")
